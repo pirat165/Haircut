8b4ceb03e619d7b980c66613c85ff45ee1b3d399
pirat165 <50583699+pirat165@users.noreply.github.com>
2022-01-12 15:46:06 +0100

    new

--- admin/admin_password_change.php
+++ admin/admin_password_change.php
@@ -1,75 +1,62 @@
-
 <?php
 
-	session_start();
-	
-	
-if ((!isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==false))
-	{
-		header('Location: index.php');
-		exit();
-	}
-   
+session_start();
 
 
-     $login = $_POST['user_login'];
+if ( ( !isset( $_SESSION[ 'zalogowany' ] ) ) && ( $_SESSION[ 'zalogowany' ] == false ) ) {
+  header( 'Location: index.php' );
+  exit();
+}
 
 
-		$haslo1 = $_POST['new_password'];
-		$haslo2 = $_POST['new_password1'];
+$login = $_POST[ 'user_login' ];
 
 
+$haslo1 = $_POST[ 'new_password' ];
+$haslo2 = $_POST[ 'new_password1' ];
 
-    require_once "connect.php";
-		mysqli_report(MYSQLI_REPORT_STRICT);
 
-        if (isset($_POST['user_login']))
-	{
-			
-            
-     if ((strlen($haslo1)<8) || (strlen($haslo1)>20))
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_pass']="Hasło musi posiadać od 8 do 20 znaków!";
-		}
-		
-		if ($haslo1!=$haslo2)
-		{
-			$wszystko_OK=false;
-			$_SESSION['e_pass2']="Podane hasła nie są identyczne!";
-		}	
-			
-try
-			{
+require_once "connect.php";
+mysqli_report( MYSQLI_REPORT_STRICT );
 
-$polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
-			if ($polaczenie->connect_errno!=0)
-			{
-				throw new Exception(mysqli_connect_errno());
-			}
-			else
-			{
-				
-				
-				//Sprawdzenie czy istnieje taki użytkownik
-				$pass_check = $polaczenie->query("SELECT ID_emp FROM employee WHERE Mail='$login'");
-				$pass = $pass_check->num_rows;
-				if (!$pass) throw new Exception_pass($polaczenie->error);
-				
-			
-					
-					if ($pass == 1)
-						
-						{
-						
-						
-						
-						if ($polaczenie->query("Update employee Set Haslo='$haslo1' WHERE Mail='$login'") ) 
-        
-					{
-						$_SESSION['udanazmiana']=true;
-						
-					echo("<script>
+if ( isset( $_POST[ 'user_login' ] ) ) {
+
+
+  if ( ( strlen( $haslo1 ) < 8 ) || ( strlen( $haslo1 ) > 20 ) ) {
+    $wszystko_OK = false;
+    $_SESSION[ 'e_pass' ] = "Hasło musi posiadać od 8 do 20 znaków!";
+  }
+
+  if ( $haslo1 != $haslo2 ) {
+    $wszystko_OK = false;
+    $_SESSION[ 'e_pass2' ] = "Podane hasła nie są identyczne!";
+  }
+
+  try {
+
+    $polaczenie = new mysqli( $host, $db_user, $db_password, $db_name );
+    if ( $polaczenie->connect_errno != 0 ) {
+      throw new Exception( mysqli_connect_errno() );
+    } else {
+
+
+      //Sprawdzenie czy istnieje taki użytkownik
+      $pass_check = $polaczenie->query( "SELECT ID_emp FROM employee WHERE Mail='$login'" );
+      $pass = $pass_check->num_rows;
+      if ( !$pass ) throw new Exception_pass( $polaczenie->error );
+
+
+      if ( $pass == 1 )
+
+      {
+
+
+        if ( $polaczenie->query( "Update employee Set Haslo='$haslo1' WHERE Mail='$login'" ) )
+
+        {
+          $_SESSION[ 'udanazmiana' ] = true;
+
+          echo( "<script>
 	
 		if (confirm('Hasło zostało zmienione pomyślnie')) {
   window.open('../admin.php', '_self');
@@ -78,45 +65,27 @@ $polaczenie = new mysqli($host, $db_user, $db_password, $db_name);
 }
   
 		
-	</script>");
-					}
-					else
-					{
-						throw new Exception($polaczenie->error);
-					}
-				
-					
-					     }
-							
-						 
-			
-				
-				$polaczenie->close();
-			}
-			}
-			catch(Exception $e)
-		{
-			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rezerwację w innym terminie!</span>';
-			echo '<br />Informacja developerska: '.$e;
-		}
-catch(Exception_pass $e)
-		{
-			echo '<span style="color:red;">Błędne hasło!</span>';
-			echo '<br />Informacja developerska: '.$e;
-		}
-
-		
-		}
-				
-				
-			
+	</script>" );
+        } else {
+          throw new Exception( $polaczenie->error );
+        }
 
 
+      }
+
+
+      $polaczenie->close();
+    }
+  } catch ( Exception $e ) {
+    echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rezerwację w innym terminie!</span>';
+    echo '<br />Informacja developerska: ' . $e;
+  } catch ( Exception_pass $e ) {
+    echo '<span style="color:red;">Błędne hasło!</span>';
+    echo '<br />Informacja developerska: ' . $e;
+  }
+
+
+}
 
 
 ?>
-
-
-
-
-
--- admin/emp_del.php
+++ admin/emp_del.php
@@ -3,78 +3,58 @@
 require_once "../connect.php";
 
 
+$id_osoby = $_SESSION[ 'ID_os' ];
+
+$AD_del = $_POST[ 'employee_del' ];
 
 
-$id_osoby = $_SESSION['ID_os'];
-
-$AD_del = $_POST['employee_del'];
+$polaczenie = @new mysqli( $host, $db_user, $db_password, $db_name );
 
 
-	$polaczenie = @new mysqli($host, $db_user, $db_password, $db_name);
+if ( $wyszukaj_admin = @$polaczenie->query( "SELECT * FROM employee WHERE Mail='$AD_del'" ) ) {
 
 
-if ($wyszukaj_admin = @$polaczenie->query("SELECT * FROM employee WHERE Mail='$AD_del'"))
-{
+  try {
+
+    if ( $polaczenie->connect_errno != 0 ) {
+      throw new Exception( mysqli_connect_errno() );
+    }
+
+    $rezultat = $polaczenie->query( "SELECT Mail FROM employee WHERE Mail='$AD_del'" );
+
+
+    if ( !$rezultat ) throw new Exception( $polaczenie->error );
+
+    $ile_takich_maili = $rezultat->num_rows;
+    if ( $ile_takich_maili < 1 ) {
+      $wszystko_OK = false;
+      echo( "Nie ma takiego użytkowinka" );
+      echo( "<A href='../admin.php'>Wróc do poprzedniej strony</A>" );
+    } else if ( $polaczenie->query( "DELETE FROM employee WHERE Mail = '$AD_del'" ) )
+
+    {
+      echo( "<script>
 	
-
-
-
-
-try 
-		{
-			
-			if ($polaczenie->connect_errno!=0)
-			{
-				throw new Exception(mysqli_connect_errno());
-			}
-	
-	          $rezultat = $polaczenie->query("SELECT Mail FROM employee WHERE Mail='$AD_del'");
-				
-				
-				if (!$rezultat) throw new Exception($polaczenie->error);
-				
-				$ile_takich_maili = $rezultat->num_rows;
-				if($ile_takich_maili<1)
-				{
-					$wszystko_OK=false;
-					echo("Nie ma takiego użytkowinka");
-					echo("<A href='../admin.php'>Wróc do poprzedniej strony</A>");
-				}		
-
-	
-			else if ($polaczenie->query("DELETE FROM employee WHERE Mail = '$AD_del'") ) 
-        
-					{
-						echo("<script>
-	
-		if (confirm('Usunięto pracownika: ".$AD_del."')) {
+		if (confirm('Usunięto pracownika: " . $AD_del . "')) {
   window.open('../admin.php', '_self');
 } else {
   window.open('../admin.php', '_self');
 }
   
 		
-	</script>");
-				
-					}
-	
-	
-	
+	</script>" );
 
+    }
+
+
+  } catch ( Exception $e ) {
+    echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rezerwację w innym terminie!</span>';
+    echo '<br />Informacja developerska: ' . $e;
+  }
+} else {
+  echo( 'Nie ma takiego użytkownika' );
 }
 
-catch(Exception $e)
-		{
-			echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rezerwację w innym terminie!</span>';
-			echo '<br />Informacja developerska: '.$e;
-		}
-}
-
-else
-	{
-	echo('Nie ma takiego użytkownika');
-	}
-
-	$polaczenie->close();
+$polaczenie->close();
 
 ?>
